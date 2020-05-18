9ae2b48c0005d9116e2f14c617bed40e652919f8
mohamed mahmoud said <65323118+mohamed-mahmoud377@users.noreply.github.com>
2020-05-19 00:37:32 +0200

    dsf

--- src/GOFO/main.java
+++ src/GOFO/main.java
@@ -1,8 +1,54 @@
 package GOFO;
 
+import GOFO.Users.Admin;
+import GOFO.Users.PlayGroundOwner;
+import GOFO.Users.Player;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+            Scanner scanner = new Scanner(System.in);
+            String string_input;
+            int int_input = 0;
+            Player currentPlayer = null;
+            PlayGroundOwner currentOwner = null;
+            Admin admin = null;
+            while(true) {
+                while (true) {
+                    try {
+                        System.out.println("welcome to our PlayGround registration system :D\n");
+                        System.out.println("[1] log in");
+                        System.out.println("[2] sing up\n");
+                        System.out.println("[3] want to own a playground ?\n");
+
+                        System.out.println("enter choose :");
+                        int_input = scanner.nextInt();
+
+                        if (int_input == 1) {
+
+
+
+                        } else if (int_input == 2) {
+
+
+                        } else if (int_input == 3) {
+
+
+                        } else {
+                            System.out.println("please enter a valid integer input ");
+                        }
+
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("sorry you entered a wrong input  \n");
+                        scanner.nextLine();
+
+                    }
+                }
+            }
     }
 }
