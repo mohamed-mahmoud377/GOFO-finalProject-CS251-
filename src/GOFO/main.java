a849a743b539baf7f58527d8ff3de78766743c03
mohamed mahmoud said <65323118+mohamed-mahmoud377@users.noreply.github.com>
2020-05-20 00:23:52 +0200

    latest updates

--- src/GOFO/main.java
+++ src/GOFO/main.java
@@ -4,6 +4,7 @@ import GOFO.DataModel.DataSource;
 import GOFO.Users.Admin;
 import GOFO.Users.PlayGroundOwner;
 import GOFO.Users.Player;
+import GOFO.Users.User;
 import GOFO.verification.Verify;
 
 import java.util.InputMismatchException;
@@ -20,6 +21,7 @@ public class Main {
             int int_input = 0;
             Player currentPlayer = null;
             PlayGroundOwner currentOwner = null;
+            User currentUser = null;
             Admin admin = null;
             while(true) {
                 while (true) {
@@ -33,83 +35,11 @@ public class Main {
                         int_input = scanner.nextInt();
 
                         if (int_input == 1) {
+                            currentUser = LogingIn();
 
                         } else if (int_input == 2) {
-                            currentPlayer = new Player();
-                            scanner.nextLine();//clean the buffer
-                            System.out.println("enter your full name ");
-                            string_input=scanner.nextLine();
 
-                            while(!currentPlayer.signUp_name(string_input)){
-                                //while get into this loop if the user entered a wrong user name
-                                //only char works
-                                System.out.println("the name entered is not valid only english char works");
-                                string_input=scanner.nextLine();
-
-                            }
-                            String playerFullName = string_input;
-//                            scanner.nextLine();
-                            System.out.println("enter your ID it (must be unique one) :  ");
-                            string_input= scanner.nextLine();
-                            while(!currentPlayer.signUp_ID(string_input)){
-                                System.out.println("the ID you entered is not valid or maybe taken");
-                                string_input=scanner.nextLine();
-
-                            }
-                            System.out.println("accepted \n your ID is : " + string_input);
-                            String PlayerID= string_input;
-//                            scanner.nextLine();
-                            System.out.println("enter you password :");
-                            string_input= scanner.nextLine();
-                            while(!currentPlayer.signUp_password(string_input)){
-                                System.out.println("the password you entered is not valid \n" +
-                                        "add at least one number and one capital alphabet and it is more than 8 character");
-                                string_input=scanner.nextLine();
-
-                            }
-
-                            while(true){
-                            System.out.println("enter your Gmail : ");
-//                            scanner.nextLine();
-                            string_input= scanner.nextLine();
-                            while(!currentPlayer.signUp_Email(string_input)){
-                                System.out.println("the entered email is not valid");
-                                System.out.println("enter you Gmail :");
-                                string_input = scanner.nextLine();
-                            }
-                                System.out.println("sending");
-                            while(!Verify.send_verify_code(string_input,playerFullName)){
-                                System.out.println("something went wrong check you internet connection or try to enter the email again");
-                                System.out.println("enter you Gmail :");
-                                string_input = scanner.nextLine();
-
-                            }
-                            System.out.println("a verification code is sent to your email");
-                            System.out.println("[1] enter the code");
-                            System.out.println("[2] resent ");
-                                  int_input= scanner1.nextInt();
-                                    if(int_input==1){
-                                        System.out.println("code :");
-                                        int_input = scanner1.nextInt();
-                                        if(!Verify.check_validation_code(int_input)){
-                                            System.out.println("the verification code is wrong please enter the Email again");
-//                                            scanner.nextLine();
-                                            continue;
-                                        }
-                                        currentPlayer.create_account();
-                                        System.out.println(" congratulations ! \nyou have registered successfully ");
-                                        break;
-
-
-
-                                    }else if(int_input ==2){
-                                        continue;
-
-                                    }else {
-                                        System.out.println("enter a valid value !");
-                                    }
-                            }
-
+                            currentPlayer= signing_up();
 
 
                         } else if (int_input == 3) {
@@ -127,5 +57,91 @@ public class Main {
                     }
                 }
             }
+    }
+    public static User LogingIn(){
+
+    }
+    public static Player signing_up(){
+        String string_input;
+        int int_input = 0;
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        Player currentPlayer = new Player();
+//        scanner.nextLine();//clean the buffer
+        System.out.println("enter your full name ");
+        string_input=scanner.nextLine();
+
+        while(!currentPlayer.signUp_name(string_input)){
+            //while get into this loop if the user entered a wrong user name
+            //only char works
+            System.out.println("the name entered is not valid only english char works");
+            string_input=scanner.nextLine();
+
+        }
+        String playerFullName = string_input;
+//                            scanner.nextLine();
+        System.out.println("enter your ID it (must be unique one) :  ");
+        string_input= scanner.nextLine();
+        while(!currentPlayer.signUp_ID(string_input)){
+            System.out.println("the ID you entered is not valid or maybe taken");
+            string_input=scanner.nextLine();
+
+        }
+        System.out.println("accepted \n your ID is : " + string_input);
+        String PlayerID= string_input;
+//                            scanner.nextLine();
+        System.out.println("enter you password :");
+        string_input= scanner.nextLine();
+        while(!currentPlayer.signUp_password(string_input)){
+            System.out.println("the password you entered is not valid \n" +
+                    "add at least one number and one capital alphabet and it is more than 8 character");
+            string_input=scanner.nextLine();
+
+        }
+
+        while(true){
+            System.out.println("enter your Gmail : ");
+//                            scanner.nextLine();
+            string_input= scanner.nextLine();
+            while(!currentPlayer.signUp_Email(string_input)){
+                System.out.println("the entered email is not valid");
+                System.out.println("enter you Gmail :");
+                string_input = scanner.nextLine();
+            }
+            System.out.println("sending");
+            while(!Verify.send_verify_code(string_input,playerFullName)){
+                System.out.println("something went wrong check you internet connection or try to enter the email again");
+                System.out.println("enter you Gmail :");
+                string_input = scanner.nextLine();
+
+            }
+            System.out.println("a verification code is sent to your email");
+            System.out.println("[1] enter the code");
+            System.out.println("[2] resent ");
+            int_input= scanner1.nextInt();
+            if(int_input==1){
+                System.out.println("code :");
+                int_input = scanner1.nextInt();
+                if(!Verify.check_validation_code(int_input)){
+                    System.out.println("the verification code is wrong please enter the Email again");
+//                                            scanner.nextLine();
+                    continue;
+                }
+                currentPlayer.create_account();
+                System.out.println(" congratulations ! \nyou have registered successfully ");
+                DataSource.getInstance().printPlayers();
+                break;
+
+
+
+            }else if(int_input ==2){
+                continue;
+
+            }else {
+                System.out.println("enter a valid value !");
+            }
+        }
+
+    return currentPlayer;
     }
 }
